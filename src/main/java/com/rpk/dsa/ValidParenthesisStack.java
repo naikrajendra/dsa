05d67262a40e541ee48e972d021ef460ebf53886
dsa <naik.raendra05@gmail.com>
2026-07-27 20:48:58 -0700

    Added more documentation

--- src/main/java/com/rpk/dsa/ValidParenthesisStack.java
+++ src/main/java/com/rpk/dsa/ValidParenthesisStack.java
@@ -6,6 +6,15 @@ package com.rpk.dsa;
  * 1. Open brackets must be closed by the same type of brackets.    
  * 2. Open brackets must be closed in the correct order.
  * 
+ * Given a string s containing just the characters '(', ')', '{', '}', '[' and ']', 
+ * determine if the input string is valid.An input string is valid if:Opening brackets 
+ * must be closed by the same type of brackets.Opening brackets must be closed in the 
+ * correct order.Every close bracket has a corresponding open bracket of the same type.
+ * Examples
+ * Example 1:Input: s = "()"Output: true
+ * Example 2:Input: s = "()[]{}"Output: true
+ * Example 3:Input: s = "(]"Output: false
+ * Example 4:Input: s = "([)]"Output: false
  * ValidParenthesisStack
  */
 
@@ -19,6 +28,8 @@ public class ValidParenthesisStack {
         } else {
             System.out.println("The parentheses are not valid.");
         }
+
+
     }
 
     public static boolean isValid(String s) {
