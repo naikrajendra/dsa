10bc13451355a77d5c40cb3bc180ae09cbb78214
naikrajendra <naik.rajendra05@gmail.com>
2026-08-22 14:51:40 -0700

    Changed input

--- src/main/java/com/hackerrank/warmup/MinimumJumpsOnClouds.java
+++ src/main/java/com/hackerrank/warmup/MinimumJumpsOnClouds.java
@@ -1,5 +1,8 @@
 package com.hackerrank.warmup;
 
+import java.util.Arrays;
+import java.util.List;
+
 /**
  * Solution for the "Minimum Jumps on Clouds" problem.
  * MinimimJumpsOnClouds
@@ -7,14 +10,14 @@ package com.hackerrank.warmup;
 
 public class MinimumJumpsOnClouds {
 
-    static int minimumJumpsOnClouds(int[] c) {
-        int n = c.length;
+    static int minimumJumpsOnClouds(List<Integer> c) {
+        int n = c.size();
         int jumps = 0;
         int i = 0;
 
         while (i < n - 1) {
             // Try to make a jump of 2 if possible, otherwise jump 1
-            if (i + 2 < n && c[i + 2] == 0) {
+            if (i + 2 < n && c.get(i + 2) == 0) {
                 i += 2;
             } else {
                 i += 1;
@@ -25,8 +28,8 @@ public class MinimumJumpsOnClouds {
         return jumps;
     }
 
-    public static void main(String[] args) {    
-        int[] c = {0, 0, 1, 0, 0, 1, 0};
+    public static void main(String[] args) {
+        List<Integer> c = Arrays.asList(0, 0, 1, 0, 0, 1, 0);
         int result = minimumJumpsOnClouds(c);
         System.out.println(result); // Expected output: 4
     }
